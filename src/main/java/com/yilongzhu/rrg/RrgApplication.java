19efa270da2ae737604800a2ca181ced1a6920ed
Yilong Zhu <me@yilongzhu.com>
2019-04-15 23:06:43 -0400

    create basic Spring/Gradle project structure
    Initial Dependencies:
    - Web
    - Security
    - DevTools

---
+++ src/main/java/com/yilongzhu/rrg/RrgApplication.java
@@ -0,0 +1,13 @@
+package com.yilongzhu.rrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrgApplication.class, args);
+	}
+
+}
